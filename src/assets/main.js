8e727b331d6593c70a2580b77d296cd925a5ba82
Eldin <eldingraho@gmail.com>
2016-12-12 20:39:23 +0100

    done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
+var a;
 $(function() {
 
   // your code will go here
+ $.ajax({
+    url: 'https://www.codeschool.com/users/300282.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      a = response;
+      console.log('api call OK!');
+      response.courses.completed.forEach(function(element) {
+        $("#badges").append('<div class="course"><h3>'+element.title+'</h3><img src="'+element.badge+'" alt="badge"><a href="'+element.url+' target="_blank" class="btn btn-primary">See Course</a></div>');
+      }, this);
+    }
+  });
 
 });
